930a32eb13ede6b28572d78348040dbb53ee3c6e
tarasovvv <tarasov_vas@mail.ru>
2025-05-18 01:39:30 +0300

    minor

--- app/actions/fetchIngredients.ts
+++ app/actions/fetchIngredients.ts
@@ -1,21 +1,21 @@
-"use server"
+// "use server"
 
-import { db, product_ingredients, productImages } from "@/src/db";
-import { eq } from "drizzle-orm";
+// import { db, product_ingredients, productImages } from "@/src/db";
+// import { eq } from "drizzle-orm";
 
-interface Ingredient {
-    name: string,
-    amount: number,
-    stock: number,
-    unit: string,
-    status: string,
-    alert: number
-}
+// interface Ingredient {
+//     name: string,
+//     amount: number,
+//     stock: number,
+//     unit: string,
+//     status: string,
+//     alert: number
+// }
 
-export async function fetchIngredients(): Promise<{ingredients: Ingredient[], error: string | null}>{
-    try{
-        const dbIngredients = await db.selectDistinct().from(product_ingredients)
+// export async function fetchIngredients(): Promise<{ingredients: Ingredient[], error: string | null}>{
+//     try{
+//         const dbIngredients = await db.selectDistinct().from(product_ingredients)
 
         
-    }
-};
+//     }
+// };
