ddafb1eee5c367e5f8c3025937df9e6d263fe67c
tarasovvv <tarasov_vas@mail.ru>
2025-05-19 12:31:47 +0300

    minor change to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['s3.diploma.larek.tech']
+    remotePatterns: ['s3.diploma.larek.tech']
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
