8398961294736c767c92c4ff1b7e73f8bad7711a
tarasovvv <tarasov_vas@mail.ru>
2026-04-19 12:22:22 +0300

    eslint config patch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,6 @@ export default [
             'quotes': ['error', 'single', {avoidEscape: true, allowTemplateLiterals: true}],
             'jsx-quotes': ['error', 'prefer-single'],
             'indent': ['error', 'tab', {SwitchCase: 1}],
-            'no-mixed-spaces-and-tabs': 'error',
             'no-tabs': 'off',
             'react-hooks/exhaustive-deps': 'error',
         },
