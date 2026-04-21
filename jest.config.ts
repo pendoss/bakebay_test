edcb7d320286caef3f245cbb0689e93a83d602c7
tarasovvv <tarasov_vas@mail.ru>
2026-04-21 21:43:08 +0300

    mobx: tests fix

--- jest.config.ts
+++ jest.config.ts
@@ -2,22 +2,23 @@ import type {Config} from 'jest'
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
 })
- 
+
 // Add any custom config to be passed to Jest
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
     moduleNameMapper: {
         '^react-markdown$': '<rootDir>/test-utils/react-markdown-mock.tsx',
         '^@/(.*)$': '<rootDir>/$1',
     },
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/tests/e2e/'],
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['/node_modules/', '/.next/', '/tests/e2e/'],
+    setupFiles: ['<rootDir>/jest.setup.js'],
+    // Add more setup options before each test is run
+    // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
- 
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
